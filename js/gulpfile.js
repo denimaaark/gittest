95fe725f46fe36f3298721e575b56525d462eb2d
deni <denimaaark@gmail.com>
2016-01-23 11:31:45 +0100

    removed gulp-webserver

--- js/gulpfile.js
+++ js/gulpfile.js
@@ -2,19 +2,6 @@ var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 var browserSync = require('browser-sync').create();
  
-// ovo radi ali sporo, osvježava se svakih 5 sekundi 
-gulp.task('webserver', function() {
-  gulp.src("./", { interval: 500 })
-    .pipe(webserver({
-      port: 8888,
-      livereload: true,
-      directoryListing: false,
-      open: true,
-      fallback: "index.html"
-    }));
-});
-
-// Static server
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
