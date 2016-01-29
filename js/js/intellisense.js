5defd63f8ab8f0ed7cd48f52cf8bd1ec6439886b
“denimaaark” <denimaaark@gmail.com>
2016-01-29 16:12:46 +0100

    added file intellisense

--- js/js/intellisense.js
+++ js/js/intellisense.js
@@ -1,6 +1,19 @@
+// autocomplete / suggestion / intellisense
+
 function fnVani(ime, prezime){
     return ime + prezime;
 }
 
 fnVani("pero", "peric");
 
+function Test(phone, mobile){
+    var address = "USA";
+    this.name = "john";
+    this.surename = "deer"
+    this.phone = phone;
+    this.mobile = mobile;
+}
+
+var t = new Test("11111", "22222");
+//t.
+//Test.
